016fb60401bb3b74dfb1be16d2d2d036c270b4c3
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-08 00:12:48 +0000

    Add files via upload

---
+++ cap12-exercicios-teoricos.py
@@ -0,0 +1,27 @@
+'''Exercícios Teóricos | Capítulo 12
+
+1) O que são e para que servem os métodos especiais?
+
+Métodos especiais, ou métodos mágicos em Python, são métodos predefinidos em todos os objetos,
+com invocação automática sob circunstâncias especiais. Eles normalmente não são chamados diretamente
+pelo usuário mas podem ser overloaded (sobrescritos e alterados). Seus nomes começam e terminam com
+sublinhados duplos chamados de dunder (uma expressão derivada de double underscore). As operações abaixo
+são exemplos de métodos mágicos e como acioná-los, com o operador + e a função len().
+
+Vemos que somar dois números usando o operador + aciona o método __add __ e
+calcular o comprimento de um objeto usando a função len() equivale a usar seu método __len__().
+
+Uma das principais vantagens de usar métodos mágicos é a possibilidade de elaborar
+classes com comportamentos similares ou iguais aos de tipos internos.
+
+
+2) Que vantagens há em esconder a representação dos objetos de uma classe do utilizador?
+
+
+3) O que se entende por interface pública?
+
+4) Que tipos de dados lineares conhece? E não lineares?
+
+5) O que é e para que serve o módulo ctypes?
+
+'''
